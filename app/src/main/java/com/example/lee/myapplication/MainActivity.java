560056f636f0717391e85029a22052d0a6c41f20
changgeunlee <dlckdrms89@naver.com>
2018-10-25 22:30:40 +0900

    화면 이동 및 버튼 생성

--- app/src/main/java/com/example/lee/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lee/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.lee.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(this, LoadingActivity.class);
         startActivity(intent);
+    }
+
+    public void onButton1Clicked(View v) {
+        Toast.makeText(getApplicationContext(), "축심!!", Toast.LENGTH_LONG).show();
+
+    }
+
+    public void onButton2Clicked(View v) {
+        Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+        startActivity(intent);
+        //Toast.makeText(getApplicationContext(), "메뉴 이동!", Toast.LENGTH_LONG).show();
     }
 }
